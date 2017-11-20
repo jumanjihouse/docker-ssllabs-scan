b1be7bffcd706436c61e3b9f6ba442d5107a9b92
Paul Morgan <jumanjiman@gmail.com>
2017-11-20 02:33:51 +0000

    ci: add shellcheck and fix offenses

--- ci/functions.sh
+++ ci/functions.sh
@@ -1,29 +1,79 @@
-verbosity=0
+# shellcheck shell=bash
+
+################################################################################
+# This collection of helper functions is sourced by the ci scripts.
+################################################################################
+
+# Defaults.
+declare -i VERBOSITY=${VERBOSITY:-1}
+
+smitty() {
+  echo; echo
+  echo "SMITTY: $*"
+  "$@"
+}
 
 err() {
-  echo ERROR: $* >&2
+  echo ERROR: "$*" >&2
 }
 
 info() {
-  [[ ${verbosity} -ge 1 ]] && echo INFO: $* || :
-}
-
-debug() {
-  [[ ${verbosity} -ge 2 ]] && echo DEBUG: $* || :
-}
-
-finish() {
-  if [[ $? -eq 0 ]]; then
-    info OK
-  else
-    err One or more failures
+  if [[ ${VERBOSITY} -ge 1 ]]; then
+    echo INFO: "$*" >&2
   fi
 }
 
-trap finish EXIT
+warn() {
+  if [[ ${VERBOSITY} -ge 1 ]]; then
+    echo WARN: "$*" >&2
+  fi
+}
 
-git_dir="$(git rev-parse --show-toplevel)"
-if ! [[ "$(pwd)" = "${git_dir}" ]]; then
-  err Please run these scripts from the root of the repo
-  exit 1
-fi
+debug() {
+  if [[ ${VERBOSITY} -ge 2 ]]; then
+    echo DEBUG: "$*" >&2
+  fi
+}
+
+finish() {
+  declare -ri RC=$?
+
+  if [ ${RC} -eq 0 ]; then
+    info "$0" OK
+  else
+    err "$0" failed with exit code ${RC}
+    exit ${RC}
+  fi
+}
+
+handle_err() {
+  declare -ri RC=$?
+
+  # $BASH_COMMAND contains the command that was being executed at the time of the trap
+  # ${BASH_LINENO[0]} contains the line number in the script of that command
+  err "exit code ${RC} from \"${BASH_COMMAND}\" on line ${BASH_LINENO[0]}"
+
+  exit ${RC}
+}
+
+is_ci() {
+  # Are we running in hands-free CI?
+  [[ -n "${CIRCLECI:-}" ]]
+}
+
+check_top_dir() {
+  declare git_dir
+  git_dir="$(git rev-parse --show-toplevel)"
+  readonly git_dir
+
+  if ! [[ "$PWD" == "${git_dir}" ]]; then
+    err Please run these scripts from the root of the repo
+    exit 1
+  fi
+}
+
+# Traps.
+# NOTE: In POSIX, beside signals, only EXIT is valid as an event.
+#       You must use bash to use ERR.
+trap finish EXIT
+trap handle_err ERR
